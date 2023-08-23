89bab8f82200c20c35fa80eb2e503181d9c75c78
sayeed <sayeed.705@gmail.com>
2023-08-24 02:53:26 +0530

    can convert multiple files at the same time,the output will be mp4

--- index.py
+++ index.py
@@ -1,46 +1,52 @@
 import tkinter as tk
 from tkinter import filedialog
 import ffmpeg
+import os
 
 def convert():
-    input_file = source_file_entry.get()
+    input_files = source_file_entry.get().split(';')
 
-    while not input_file:
-        print('Input filename is not entered \n')
-        log_messages.insert(tk.END,'Input filename is not entered \n')
-    output_file =output_file_entry.get()
+    while not input_files[0]:
+        print('Input filename(s) are not entered')
+        log_messages.insert(tk.END, 'Input filename(s) are not entered \n')
+        return
 
-    while not output_file:
-        print('Output filename is not entered')
-        log_messages.insert(tk.END,'Output filename is not entered \n')
+    for input_file in input_files:
+        try:
+            # Get the input file's directory and file name without extension
+            input_folder = os.path.dirname(input_file)
+            file_name = os.path.splitext(os.path.basename(input_file))[0]
+            output_file = os.path.join(input_folder, f'{file_name}.mp4')
 
+            # Run the conversion
+            ffmpeg.input(input_file).output(output_file, acodec='copy', vcodec='copy').run()
+        except Exception as e:
+            # Print an error message
+            print(f'Error: {e}')
+            log_messages.insert(tk.END, f'Error: {e} \n')
+        else:
+            # Print a success message
+            print(f"Conversion of {input_file} complete!")
+            log_messages.insert(tk.END, f"Conversion of {input_file} complete! \n")
 
-    try:
-        # Run the conversion
-        ffmpeg.input(input_file).output(output_file,acodec='copy',vcodec='copy').run()
-    except ffmpeg.Error as e:
-        # Print an error message
-        print(f'\033[0;31m Error: {e}')
-        log_messages.insert(tk.END,f'\033[0;31m Error: {e} \n')
-    else:
-        # Print a success message
-        print("\033[1;32m Conversion complete!  \n")
-        log_messages.insert(tk.END,"\033[1;32m Conversion complete!  \n")
+def browse_files():
+    file_paths = filedialog.askopenfilenames()
+    if file_paths:
+        formatted_paths = ';'.join(file_paths)
+        source_file_entry.delete(0, tk.END)  # Clear any existing content
+        source_file_entry.insert(tk.END, formatted_paths)
 
 # Create the main window
 window = tk.Tk()
 window.title('ffmpeg - converter')
-window.geometry('400x400')
+window.geometry('600x400')
 
 # Create the label and entry widgets for the file input
-source_file_label = tk.Label(window, text='Select a file:')
+source_file_label = tk.Label(window, text='Enter input filenames (separated by ;):')
 source_file_entry = tk.Entry(window)
-# Create the button widget for the file browser
-source_file_location = tk.Button(window, text='Browse', command=lambda: source_file_entry.insert(0, filedialog.askopenfilename()))
 
-# Create the label and entry widgets for the second input
-output_file_label = tk.Label(window, text='Enter Output Filename with format (Eg:output.mp4):')
-output_file_entry = tk.Entry(window)
+# Create the button widget for the file browser
+source_file_location = tk.Button(window, text='Browse', command=browse_files)
 
 # Create the button widget for printing the values
 print_button = tk.Button(window, text='Convert', command=convert)
@@ -52,14 +58,8 @@ log_messages = tk.Text(window)
 source_file_label.pack()
 source_file_entry.pack()
 source_file_location.pack()
-output_file_label.pack()
-output_file_entry.pack()
 print_button.pack()
 log_messages.pack()
 
 # Run the main loop
 window.mainloop()
-
-
-
-
